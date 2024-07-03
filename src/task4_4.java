ecd7b1d84de75762e87027de39245f1440318a75
YardaRum <166068579+YardaRum@users.noreply.github.com>
2024-07-03 20:05:47 +0300

    ...

--- src/task4_4.java
+++ src/task4_4.java
@@ -1,20 +1,21 @@
-import java.text.DateFormat;
 import java.text.*;
 import java.util.Date;
-
+import java.util.Scanner;
+import java.text.ParseException;
 
 public class task4_4 {
     public static void main(String[] args) {
+        System.out.println("Введите дату в формате 'дд.мм.гггг'");
+        Scanner scan = new Scanner(System.in);
+        String STR = scan.nextLine();
         Date date = new Date();
-        System.out.println("Текущее время в формате 'дд.мм.гггг'");
         SimpleDateFormat SDF= new SimpleDateFormat("dd.MM.yyyy");
-        System.out.println(SDF.format(date));
-        System.out.println("Текущее время в формате 'гггг-мм-дд'");
+        try {
+            date = SDF.parse(STR);
+        } catch(ParseException e) {
+            e.printStackTrace();
+        }
         SimpleDateFormat SDF1= new SimpleDateFormat("yyyy-MM-dd");
         System.out.println(SDF1.format(date));
-
-
-
-
     }
 }
